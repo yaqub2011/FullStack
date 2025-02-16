3c290d6ec06cb706ef065948a627ac61689dfaff
Yaqub <yaqubmamme95@gmail.com>
2025-02-16 09:06:04 +0400

    HwC++

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+// Zadaniye hw prededujiy
+
+//    int total_seconds;
+//    cout << "Enter the number of seconds: ";
+//    cin >> total_seconds;
+//
+//    int hours = total_seconds / 3600;
+//    int minutes = (total_seconds % 3600) / 60;
+//    int seconds = total_seconds % 60;
+//
+//    cout << "Hours: " << hours << " Minutes: " << minutes << " Seconds: " << seconds << endl;
+//    return 0;
+
+// Zadaniye Hw Noviy nomer 1
+
+//double num1, num2;
+
+//cout << "Enter your first number: ";
+//cin >> num1;
+//cout << "Enter your second number: ";
+//cin >> num2;
+
+//double sum = num1 + num2;
+//double product = num1 * num2;
+//double average = sum / 2;
+
+//cout << "Sum: " << sum << endl;
+//cout << "multiplication: " << product << endl;
+//cout << "Arithmetic: " << average << endl;
+
+//return 0;
+
+// Zadaniye Nomer 2
+
+//double num1, num2, num3;
+//
+//cout << "Enter your first number: ";
+//cin >> num1;
+//cout << "Enter your second number: ";
+//cin >> num2;
+//cout << "Enter your third number: ";
+//cin >> num3;
+//
+//double sum = num1 + num2 + num3;
+//double multiplication = num1 * num2 * num3;
+//double arithmetic = sum / 3;
+//
+//cout << "Sum: " << sum << endl;
+//cout << "multiplication: " << multiplication << endl;
+//cout << "Arithmetic: " << arithmetic << endl;
+//
+//return 0;
+
+// Nomer 3
+ 
+//double price;
+//int stock;
+//double discount;
+
+//cout << "Enter the price of one laptop: ";
+//cin >> price;
+//cout << "Enter the quantity of laptops: ";
+//cin >> stock;
+//cout << "Enter the discount percentage: ";
+//cin >> discount;
+
+//double Total = price * stock;
+//double stockAmount = Total * (discount / 100);
+//double TotalPrice = Total - stockAmount;
+
+//cout << "Total order amount after discount: " << TotalPrice << endl;
+
+//return 0;
+}
