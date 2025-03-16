887d705235e42a52e3c30d7af6cfeeab0af77db2
Yaqub <yaqubmamme95@gmail.com>
2025-03-16 09:13:55 +0400

    ConsoleAPPP3

--- ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,35 +1,20 @@
-//#include <iostream>
+// ConsoleApplication3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-//using namespace std;
-//
-//void test1(int num[], int i) {
-//    int a = 0;
-//    while (a < i) {
-//        if (num[a] % 2 == 0) {
-//            cout << num[a] << endl;
-//        }
-//        a++;
-//    }
-//}
-//void test2() {
-//
-//}
-//
-//int main() {
-//    int numbers[6] = { 1, 2, 3, 4, 5, 6 };
-//    test1(numbers, 6);
-//}
-//void test2(int numbers[], int Aftondil) {
-//    int sum = 0;
-//    int i = 0;
-//    while (i < Aftondil) {
-//        sum += numbers[i];
-//        i++;
-//    }
-//    cout << "Sum of array elements: " << sum << endl;
-//}
-//
-//int main() {
-//    int numbers[6] = { 1, 2, 3, 4, 5, 6 };
-//    test2(numbers, 6);
-//}
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
