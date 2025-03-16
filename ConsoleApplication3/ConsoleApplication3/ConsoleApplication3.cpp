f99904919d0fe1d0868e70694981962d525ad105
Yaqub <yaqubmamme95@gmail.com>
2025-03-16 09:04:31 +0400

    ConsoleApp3

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//void test1(int num[], int i) {
+//    int a = 0;
+//    while (a < i) {
+//        if (num[a] % 2 == 0) {
+//            cout << num[a] << endl;
+//        }
+//        a++;
+//    }
+//}
+//void test2() {
+//
+//}
+//
+//int main() {
+//    int numbers[6] = { 1, 2, 3, 4, 5, 6 };
+//    test1(numbers, 6);
+//}
+//void test2(int numbers[], int Aftondil) {
+//    int sum = 0;
+//    int i = 0;
+//    while (i < Aftondil) {
+//        sum += numbers[i];
+//        i++;
+//    }
+//    cout << "Sum of array elements: " << sum << endl;
+//}
+//
+//int main() {
+//    int numbers[6] = { 1, 2, 3, 4, 5, 6 };
+//    test2(numbers, 6);
+//}
