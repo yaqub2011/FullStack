054d372cfca9d68c18c2155d15499025f5aee1eb
Yaqub <yaqubmamme95@gmail.com>
2025-04-26 09:12:25 +0400

    'repeat'

---
+++ Repeat/Repeat/Repeat.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//int num = 0;
+	//while (num <= 10)
+	//{
+	//	cout << num << endl;
+	//	num++;
+	//}
+	
+	
+	//int num1 = 2;
+	//while(num1 <= 20)
+	//{
+	//	cout << num1 << endl;
+	//	num1 += 2;
+	//}
+
+
+	/*int num = 0;
+	int num1 = 1;
+	cout << "Enter ur number: ";
+	cin >> num;
+	while (num1 <= num) {
+		cout << num1 << endl;
+		num1++;
+	}*/
+	//int N;
+	//cout << "input number: ";
+	//cin >> N;
+
+	//int M = 1;
+	//int sum = 0;
+
+	//while (M <= N) {
+	//	sum += M;
+	//	M++;
+	//}
+
+	//cout << "sum 1 to" << " " << N << " = " << sum << endl;
+	/*int N;
+	cout << "Enter a number N: ";
+	cin >> N;
+
+	int i = 1;
+	int factorial = 1;
+
+	while (i <= N) {
+		factorial *= i;
+		i++;
+	}
+
+	cout << "The factorial  " << N << " is " << factorial << endl;*/
+}
